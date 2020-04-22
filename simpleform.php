f802384910bdd173aea87b57028a1e5a2f59866a
Dennis Yang <dennishyang@gmail.com>
2020-04-22 14:48:02 -0400

    First Commit

---
+++ simpleform.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>PHP example</title>
+</head>
+
+<body>
+  <form action="simpleform.php" method="post">
+    <input type="text" name="yourname" /> <br/>
+    <input type="submit" value="Submit" />
+  </form>
+  
+<?php
+$str = "Hello world"; 
+if (isset($_POST['yourname']))
+   $str = "You've entered ". $_POST['yourname'];
+echo $str;  
+?>
+
+</body>
+</html>   
