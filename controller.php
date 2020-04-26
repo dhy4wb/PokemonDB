2d1b6d564532038769512d43d7a76c64c27e8a99
Zeshu Zhao <33557220+Pucch0@users.noreply.github.com>
2020-04-26 14:46:51 -0400

    Add files via upload

--- controller.php
+++ controller.php
@@ -30,7 +30,10 @@ switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
 				break;
 		case '/logout':
 				require 'logout.php';
-				break;
+                break;
+    case '/friend':
+        require 'friend.php';
+        break;
     default:
         http_response_code(404);
         exit('Not Found');
